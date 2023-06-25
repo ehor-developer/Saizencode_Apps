60183ff83b3726f6af82e67e5499b3f22e4e8b64
Ehor <123355005+Ehor-developer@users.noreply.github.com>
2023-06-25 18:33:54 +0900

    ピン留め機能の追加

--- islands/app.tsx
+++ islands/app.tsx
@@ -14,6 +14,7 @@ interface SaizencodeProps {
 export default function Saizencode({ menus }: SaizencodeProps) {
   const [searchValue, setSearchValue] = useState("");
   const [filteredMenus, setFilteredMenus] = useState(menus);
+  const [favoriteMenus, setFavoriteMenus] = useState<MenuItem[]>([]);
 
   useEffect(() => {
     if (searchValue === "") {
@@ -31,6 +32,15 @@ export default function Saizencode({ menus }: SaizencodeProps) {
     }
   }, [searchValue, menus]);
 
+  const addToFavorites = (menu: MenuItem) => {
+    setFavoriteMenus((prevFavorites) => [...prevFavorites, menu]);
+  };
+  const removeFromFavorites = (menu: MenuItem) => {
+    setFavoriteMenus((prevFavorites) =>
+      prevFavorites.filter((favorite) => favorite.id !== menu.id)
+    );
+  };
+  
   return (
     <div>
       <div className="flex m-4">
@@ -44,7 +54,37 @@ export default function Saizencode({ menus }: SaizencodeProps) {
           />
         </div>
       </div>
+      <h3 class="text-lg font-bold  m-4">📌ピン留め</h3>
+      {favoriteMenus.map((favorite) => (
+        <div
+          key={favorite.id}
+          className="flex justify-between items-center bg-white p-4 mb-4 shadow-xl rounded-md font-bold"
+        >
+          <div className="flex items-center">
+            <div className="rounded-full flex justify-center items-center text-white font-bold text-3xl mr-4">
+              {favorite.emoji}
+            </div>
+            <div>
+              <p className="text-gray-500 text-sm font-bold">{favorite.id}</p>
+              <h3 className="text-lg font-bold">{favorite.name}</h3>
+            </div>
+          </div>
+          <div className="flex items-center">
+            <span className="text-lg font-bold text-gray-800 mr-2">
+              {favorite.price}円
+            </span>
+            <button
+              className="bg-red-500 rounded-full w-6 h-6 text-white"
+              onClick={() => removeFromFavorites(favorite)}
+            >
+              ×
+            </button>
+          </div>
+        </div>
+      ))}
 
+      <hr class="m-4"></hr>
+      <h3 class="text-lg font-bold m-4">🥗メニュー 一覧</h3>
       {filteredMenus.map((menuItem) => (
         <div
           key={menuItem.id}
@@ -63,6 +103,12 @@ export default function Saizencode({ menus }: SaizencodeProps) {
             <span className="text-lg font-bold text-gray-800 mr-2">
               {menuItem.price}円
             </span>
+            <button
+              className="bg-green-500 rounded-full w-6 h-6 text-white"
+              onClick={() => addToFavorites(menuItem)}
+            >
+              ＋
+            </button>
           </div>
         </div>
       ))}
